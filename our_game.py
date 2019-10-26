add0ed40dfbbd5c87d93b70a723106cbe91b3e57
natkaszop <56702205+natkaszop@users.noreply.github.com>
2019-10-26 23:11:00 +0100

    #PureBeltin

--- our_game.py
+++ our_game.py
@@ -51,10 +51,39 @@ class Our_Map():
         cells = range(32)
         positions_of_cells = dict()
 
-        positions_of_cells[0] = [,]
-
-
-
+        positions_of_cells[0] = [180,1215]
+        positions_of_cells[1] = [303,1215]
+        positions_of_cells[2] = [430,1200]
+        positions_of_cells[3] = [450,1080]
+        positions_of_cells[4] = [437,955]
+        positions_of_cells[5] = [314,936]
+        positions_of_cells[6] = [188,923]
+        positions_of_cells[7] = [140,819]
+        positions_of_cells[8] = [140,689]
+        positions_of_cells[9] = [140,558]
+        positions_of_cells[10] = [173,433]
+        positions_of_cells[11] = [298,423]
+        positions_of_cells[12] = [406,474]
+        positions_of_cells[13] = [416,600]
+        positions_of_cells[14] = [474,698]
+        positions_of_cells[15] = [600,693]
+        positions_of_cells[16] = [661,600]
+        positions_of_cells[17] = [661,468]
+        positions_of_cells[18] = [671,341]
+        positions_of_cells[19] = [792,296]
+        positions_of_cells[20] = [896,361]
+        positions_of_cells[21] = [896,486]
+        positions_of_cells[22] = [896,616]
+        positions_of_cells[23] = [896,750]
+        positions_of_cells[24] = [896,874]
+        positions_of_cells[25] = [896,1000]
+        positions_of_cells[26] = [828,1110]
+        positions_of_cells[27] = [707,1116]
+        positions_of_cells[28] = [645,1217]
+        positions_of_cells[29] = [673,1344]
+        positions_of_cells[30] = [794,1365]
+        positions_of_cells[31] = [923,1358]
+        
 def main():
     """ Main Program """
     pg.init()
